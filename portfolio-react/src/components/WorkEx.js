8a073d5ec6ad92a8d31a803359d16708b0a54b45
Jacob Goldfine <jacobjgoldfine@gmail.com>
2021-10-05 16:44:13 -0500

    added links to github repos

--- portfolio-react/src/components/WorkEx.js
+++ portfolio-react/src/components/WorkEx.js
@@ -4,22 +4,27 @@ export default function WorkEx() {
   return (
     <div>
       <article id="first_work" className="work">
-        <h2>Work</h2>
-        <h3>First Work overlay</h3>
+        <h2>Work Examples</h2>
+
         <button type="button">
-          <img src="assets/images/placeHolder1.jpg" alt="Work One Placeholder" />
+          <a href="https://github.com/jacobjgoldfine/quote_gen_translator">
+            <h3>Random Quote Generator and Translator</h3>
+            <img src="assets/images/placeHolder1.jpg" alt="Work One Placeholder" />
+          </a>
         </button>
       </article>
 
       <article className="work">
-        <h3>Second Work overlay</h3>
         <button type="button">
-          <img src="assets/images/placeHolder2.jpg" alt="Work Two Placeholder" />
+          <a href="https://github.com/jacobjgoldfine/collection_social_share">
+            <h3>Collection Social Share</h3>
+            <img src="assets/images/placeHolder2.jpg" alt="Work Two Placeholder" />
+          </a>
         </button>
       </article>
 
       <article className="work">
-        <h3>Third Work overlay</h3>
+        <h3>Project3 Goes Here</h3>
         <button type="button">
           <img src="assets/images/placeHolder3.jpg" alt="Work Three Placeholder" />
         </button>
